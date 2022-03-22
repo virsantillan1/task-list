0e7c773f30d4f81329deba3eea2dbb3a6b82f7b1
virsantillan98 <virsantillan98@hotmail.com.ar>
2022-03-22 11:46:52 -0300

    terminado

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -19,5 +19,26 @@ export class TasksComponent implements OnInit {
       this.tasks = tasks
     ));
   }
-  
+
+  deleteTask(task:Task) {
+    this.taskService.deleteTask(task)
+    .subscribe(
+      () => (
+        this.tasks = this.tasks.filter ( (t) => {
+          return t.id !== task.id
+        })
+      ));
+  }
+
+  toggleReminder(task:Task) {
+    task.reminder = !task.reminder
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task:Task) {
+    this.taskService.addTask(task).subscribe((task)=>(
+      this.tasks.push(task)
+    ))
+  }
+
 }
